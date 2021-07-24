e9afed5a9eabd5209a7463082ac780140dfca2fa
litecj <litecj702@gmail.com>
2021-07-24 15:50:19 +0900

    weeken

--- titanic/model/service.py
+++ titanic/model/service.py
@@ -11,32 +11,32 @@ class Service (object) :
         this.fname = payload
         return pd.read_csv(this.context + this.fname)
 
-def create_train(this:str) -> object:
+def create_train(this) -> {}:
     return this
 
-def create_label(this:str) -> object:
+def create_label(this) -> {}:
     return this
 
-def drop_feature(this,*feature):
+def drop_feature(this,*feature) ->(object):
     return this
 
-def embarked_nominal(this):
+def embarked_nominal(this)->(object):
     return this
 
-def fare_oridnal(this):
+def fare_oridnal(this)->(object):
     return this
 
-def title_nominal(this):
+def title_nominal(this)->(object):
     return this
 
-def gender_nirminal(this):
+def gender_nirminal(this)->(object):
     return this
 
-def age_ordinal(this):
+def age_ordinal(this)->(object):
     return this
 
-def create_k_fold(this:object) -> {}:
+def create_k_fold(this) -> {}:
     return None
 
-def accuracy_by_classfier(this):
+def accuracy_by_classfier(this) -> str:
     return ""
